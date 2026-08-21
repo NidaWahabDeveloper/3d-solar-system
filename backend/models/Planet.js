3bad1d8b2de18df0658d04021fe739826b0f6cc4
Nida Wahab <savepakistan1486@gmail.com>
2026-08-21 10:18:45 +0500

    Add MongoDB connection and Planet model

---
+++ backend/models/Planet.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+const factSchema = new mongoose.Schema (
+    {
+        label: { type: string, required: true},
+        value: { type: string, required: true }
+    },
+    { id_: false} 
+);
+const planetSchema = new mongoose.Schema (
+    {
+        name: {
+            type: String,
+            required: [ true, 'Planet name is required.'],
+            unique: true,
+            trim: true,
+        },
+
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        tagline: {
+            type: String,
+            default: "",
+        },
+        description: {
+            type: String,
+            required: [ true, "Hey, Nida! Description is required here."],
+        },
+        color: {
+             type: String,
+      default: "#5B8DEF",
+        },
+        textureUrl: {
+            type: String,
+      default: "",
+        },
+
+        orbitPosition: {
+            type: Number,
+            required: true,
+        },
+
+        facts: [factSchema],
+
+        funFact: {
+            type: String,
+            default: "",
+        },
+
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+
+    { timestamps: true }
+);
+
+planetSchema.index( { name: "text" , description: "text" } );
+export default mongoose.model( "Planet", planetSchema );
